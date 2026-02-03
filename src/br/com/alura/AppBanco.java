2f52f48bbfd3d2eb8e9c3d84b4964b64bfad738b
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-03 10:59:38 -0300

    Added join method

--- src/br/com/alura/AppBanco.java
+++ src/br/com/alura/AppBanco.java
@@ -18,6 +18,15 @@ public class AppBanco {
 
         System.out.println(Thread.currentThread().getName());
 
+        try {
+            saqueJoao.join();
+            saqueMaria.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Saldo final: " + conta.getSaldo() );
+
 //        //saque João
 //        operacao.executa();
 //        //saque Maria
